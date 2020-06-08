e46da4440b9046b6ed0e721baf53a435aa7133f9
Daniel Mendez <dmtx97@gmail.com>
2020-06-07 21:49:06 -0500

    fixed discord_base indent

--- discord_base.py
+++ discord_base.py
@@ -5,17 +5,18 @@ from classes.discbot import DiscCog, Users
 
 
 if __name__ == "__main__":
+    
 
-bot = commands.Bot(command_prefix=commands.when_mentioned_or("!"),
-                description='Joke Bot!')
+    bot = commands.Bot(command_prefix=commands.when_mentioned_or("!"),
+                    description='Joke Bot!')
 
-@bot.event
-async def on_ready():
-    print('Logged in as {0} ({0.id})'.format(bot.user))
-    print('------')
+    @bot.event
+    async def on_ready():
+        print('Logged in as {0} ({0.id})'.format(bot.user))
+        print('------')
 
 
-bot.add_cog(DisCog(bot))
+    bot.add_cog(DisCog(bot))
 
-bot.loop.create_task(get_guilds())
-bot.run(discord_token)
+    bot.loop.create_task(get_guilds())
+    bot.run(discord_token)
